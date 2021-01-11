b78c192ee9a86d5cf82038d83088bfe80aaa6d93
unknown <mitulgarg2000@gmail.com>
2021-01-11 20:09:30 +0530

    Heart and instruction added

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 var count = 0
-$("#love").click(function() {
+$("#heart").click(function() {
     count++;
     if(count == 1) {
         $("#love").text("L");
     }
     else if(count == 2){
-        $("#love").text("O");
+        $("#love").text("LO");
     }
     else if(count == 3){
-        $("#love").text("V");
+        $("#love").text("LOV");
     } 
     else if(count == 4) {
-        $("#love").text("E");
+        $("#love").text("LOVE");
     }
     else if(count == 5) {
         $("#love").text("Y");
     }
     else if(count == 6) {
-        $("#love").text("O");
+        $("#love").text("YO");
     }
     else if(count == 7) {
-        $("#love").text("U");
+        $("#love").text("YOU");
     }
     else if(count == 8) {
         $("#love").text("I LOVE YOU");
+        $("#click").text("");
     }
 })
